c764770d312f230490d9951747d4c43b9fab8fee
LvInSaNevL <33157232+LvInSaNevL@users.noreply.github.com>
2018-10-17 18:36:43 -0400

    Checksum and new JSON
    I added in 5 checksums for each game and created `gameData_min.json` which is just the appid and name for each game

--- skynet-style/js/downloads/gamedata.js
+++ skynet-style/js/downloads/gamedata.js
@@ -5,6 +5,13 @@ var GameData = [
 		"description": "Amnesia: The Dark Descent, a first person survival horror. A game about immersion, discovery and living through a nightmare. An experience that will chill you to the core.",
 		"dev": "Frictional Games",
 		"name": "57300",
+		"checksums": {
+			"CRC32": "43B0FC78",
+			"CRC64": "79E73FDC99D036D1",
+			"SHA256": "84875FF62B13606D841739BD88708C8553F1F7B6CD8574F3989C514213718F13",
+			"SHA1": "3E53C1D438D7AB63E0AB0390006F8C49A9A3D446",
+			"BLAKE2sp": "5FB4D178DF2A39127CF2AC87C2AAB145C3CFBD1B353654B42CF0AC503AFEDAD2"
+		},
 		"tags": [
 			"Horror",
 			"First-Person",
@@ -18,6 +25,13 @@ var GameData = [
 		"description": "Guide a group of space settlers trying to establish a base on a remote planet. Grow food, collect energy, mine resources, survive disasters and build a self-sufficient colony in a harsh and unforgiving environment.",
 		"dev": "Madruga Works",
 		"name": "403190",
+		"checksums": {
+			"CRC32": "08D50F0A",
+			"CRC64": "0898F88C0E05A912",
+			"SHA256": "9C96DFC96480A2EA70EF7F442FABFBB23222813EBF7138F763D83D20C67568FD",
+			"SHA1": "C2814EA5D692D9A91103AEA2817FA3C9B6F06264",
+			"BLAKE2sp": "F4BF99550C8DE0DAD653FAF25FA7364C56333E8309C96E2E7CD3C1148652B341"
+		},
 		"tags": [
 			"City Builder",
 			"Space",
@@ -31,6 +45,13 @@ var GameData = [
 		"description": "RollerCoaster Tycoon Deluxe takes you for a ride in the original RollerCoaster Tycoon and its expansion packs, Corkscrew Follies and Loopy Landscapes. Create the ultimate theme park using a variety of coaster types with giant loops and barrel rolls and in-park attractions like suspended monorails and water-soaked plume rides.",
 		"dev": "Chris Sawyer Productions",
 		"name": "285310",
+		"checksums": {
+			"CRC32": "7F57907E",
+			"CRC64": "6F299B85E0BA5D43",
+			"SHA256": "4FAB42A1DDE995614106E64E67E5C4A2AB523B561B59681AF1EA2CB9D6E4D58B",
+			"SHA1": "97FDB98F0DC8A7B17AD5216F3926D771DD16C57B",
+			"BLAKE2sp": "97255D63B174A84C71A748C43E58C5B76802EB59969D2FA3F0C8B71B3F484144"
+		},
 		"tags": [
 			"Simulation",
 			"Management",
@@ -44,6 +65,13 @@ var GameData = [
 		"description": "Bloody Trapland is a fast-paced multiplayer platformer where the levels are filled with dozens of dangerous traps and tricky jumps that stand in your way! Play co-op with your friends or take on the challenges on your own. With support for up to 4 players at once, local and/or online.",
 		"dev": "2Play",
 		"name": "257750",
+		"checksums": {
+			"CRC32": "FD1947DF",
+			"CRC64": "A1ECE4706EBC289B",
+			"SHA256": "6840DA6A093AB6A8681788773F162FBD409223C69281BD4469646B028788351E",
+			"SHA1": "0863345FB5E19C3890A76DED1218D1B25D9FC7FE",
+			"BLAKE2sp": "42DA85B971DC95868BB656F9004558FDA7CA81D4ABB9069767E2B871E701DC3D",
+		},
 		"tags": [
 			"Platformer",
 			"Co-op",
@@ -57,6 +85,13 @@ var GameData = [
 		"description": "From the creators of Amnesia: The Dark Descent and Dear Esther comes a new first-person horrorgame that will drag you to the depths of greed, power and madness. It will bury its snout into your ribs and it will eat your heart.",
 		"dev": "The Chinese Room",
 		"name": "239200",
+		"checksums": {
+			"CRC32": "82C24E89",
+			"CRC64": "43CCFF31E8F7304D",
+			"SHA256": "314C774E250C304F2B71FF182661A3703C2C7555CDFBF705E94A0B26640256FD",
+			"SHA1": "E7A02BDAA70E43A473DF684A385BAA7DA415B23F",
+			"BLAKE2sp": "D18ED15BB3E5011F8CD81B2F27FF2413A46DACF3C459DD2AA1F76C6126068399"
+		},
 		"tags": [
 			"Horror",
 			"Indie",
