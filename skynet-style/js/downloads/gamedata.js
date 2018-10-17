46a649b9ff6ffdd38f0b93ee01c9d21b957678e2
LvInSaNevL <33157232+LvInSaNevL@users.noreply.github.com>
2018-10-17 14:43:17 -0400

    Updated JSON
    Fixed the data on the JSON so all games show up properly

--- skynet-style/js/downloads/gamedata.js
+++ skynet-style/js/downloads/gamedata.js
@@ -1,47 +1,67 @@
 var GameData = [
-  {
-    "name": "planetbase",
-    "alt": "Planetbase",
-    "platforms": "100",
-    "date": "Oct 15, 2015",
-    "dev": "Madruga Works",
-    "pub": "Madruga Works",
-    "description": "Guide a group of space settlers trying to establish a base on a remote planet. Grow food, collect energy, mine resources, survive disasters and build a self-sufficient colony in a harsh and unforgiving environment.",
-    "tags": [
-      "City Builder",
-      "Space",
-      "Base Building",
-      "Strategy"
-    ]
-  },
-  {
-    "name": "coaster",
-    "alt": "RollerCoaster Tycoon Deluxe",
-    "platforms": "100",
-    "date": "Mar 31, 1999",
-    "dev": "Chris Sawyer Productions",
-    "pub": "Atari",
-    "description": "RollerCoaster Tycoon Deluxe takes you for a ride in the original RollerCoaster Tycoon and its expansion packs.",
-    "tags": [
-      "Simulation",
-      "Managment",
-      "Strategy",
-      "Sandbox"
-    ]
-  },
-  {
-	"name": "orwell",
-	"alt": "Orwell: Keeping an eye on you",
-	"platforms": "100",
-	"date": "Oct 27, 2016",
-	"dev": "Osmotic Studios",
-	"pub": "Fellow Traveller",
-	"description": "Big Brother has arrived - and it’s you. Investigate the lives of citizens to find those responsible for a series of terror attacks. Information from the internet, personal communications and private files are all accessible to you. But, be warned, the information you supply will have consequences.",
-	"tags": [
-		"Choices Matter",
-		"Story Rich",
-		"Indie",
-		"Dystopian"
-	]
-  }
+	{
+		"alt": "Amnesia: The Dark Descent",
+		"date": "Sep 8, 2010",
+		"description": "Amnesia: The Dark Descent, a first person survival horror. A game about immersion, discovery and living through a nightmare. An experience that will chill you to the core.",
+		"dev": "Frictional Games",
+		"name": "57300",
+		"tags": [
+			"Horror",
+			"First-Person",
+			"Atmospheric",
+			"Survival Horror"
+		]
+	},
+	{
+		"alt": "Planetbase",
+		"date": "Oct 15, 2015",
+		"description": "Guide a group of space settlers trying to establish a base on a remote planet. Grow food, collect energy, mine resources, survive disasters and build a self-sufficient colony in a harsh and unforgiving environment.",
+		"dev": "Madruga Works",
+		"name": "403190",
+		"tags": [
+			"City Builder",
+			"Space",
+			"Base Building",
+			"Strategy"
+		]
+	},
+	{
+		"alt": "RollerCoaster Tycoon: Deluxe",
+		"date": "Mar 31, 1999",
+		"description": "RollerCoaster Tycoon Deluxe takes you for a ride in the original RollerCoaster Tycoon and its expansion packs, Corkscrew Follies and Loopy Landscapes. Create the ultimate theme park using a variety of coaster types with giant loops and barrel rolls and in-park attractions like suspended monorails and water-soaked plume rides.",
+		"dev": "Chris Sawyer Productions",
+		"name": "285310",
+		"tags": [
+			"Simulation",
+			"Management",
+			"Strategy",
+			"Sandbox"
+		]
+	},
+	{
+		"alt": "Bloody Trapland",
+		"date": "Oct 27, 2013",
+		"description": "Bloody Trapland is a fast-paced multiplayer platformer where the levels are filled with dozens of dangerous traps and tricky jumps that stand in your way! Play co-op with your friends or take on the challenges on your own. With support for up to 4 players at once, local and/or online.",
+		"dev": "2Play",
+		"name": "257750",
+		"tags": [
+			"Platformer",
+			"Co-op",
+			"Indie",
+			"Difficult"
+		]
+	},
+	{
+		"alt": "Amnesia: A Machine for Pigs",
+		"date": "Sep 10, 2013",
+		"description": "From the creators of Amnesia: The Dark Descent and Dear Esther comes a new first-person horrorgame that will drag you to the depths of greed, power and madness. It will bury its snout into your ribs and it will eat your heart.",
+		"dev": "The Chinese Room",
+		"name": "239200",
+		"tags": [
+			"Horror",
+			"Indie",
+			"First-Person",
+			"Walking Simulator"
+		]
+	}
 ]
